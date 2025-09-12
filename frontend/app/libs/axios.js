9f60d1e796bb668b358d55a3a16b5290dd53bc10
Naimur Rahman <158295081+naimur009@users.noreply.github.com>
2025-09-12 13:07:27 +0600

    Update axios.js

--- frontend/app/libs/axios.js
+++ frontend/app/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://evently-backend-0r9f.onrender.com 
+  baseURL: "https://evently-backend-0r9f.onrender.com"
   withCredentials: true, // if you need cookies/session
 });
 
